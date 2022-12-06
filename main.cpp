82ae4763853058ed8a6efb83e7ab233db5a79c64
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-06 11:09:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char c=0;
+    cin>>c;
+    if(conv(c)==1){
+        cout<<c<<endl;
+    }else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
