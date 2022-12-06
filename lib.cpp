91cab556e63a0bc63a6e1700b64f99f5c76ac7aa
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-06 11:09:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+
+bool conv(char &c){
+    if(c>=65 and c<=90){
+        c=c+32;
+        return true;
+    }else if(c>=97 and c<=122){
+        c=c-32;
+        return true;
+    }else{
+        return false;
+    }
+}
 
